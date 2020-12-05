7cd6864e1a18a93dc0a96d3725db6c9593f17012
Kaito-Dogi <kendobu0405@gmail.com>
2020-12-05 12:14:18 +0900

    効果音実装

--- app/src/main/java/com/doggy/count/MainActivity.kt
+++ app/src/main/java/com/doggy/count/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.doggy.count
 
 import android.graphics.Color
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -15,6 +16,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //サウンドファイルを読み込んで、プレイヤーを作成
+        val plusSound = MediaPlayer.create(this, R.raw.plus_sound)
+        val minusSound = MediaPlayer.create(this, R.raw.minus_sound)
+
         //TextViewに初期値を表示
         count_text_view.text = "0"
 
@@ -25,10 +30,15 @@ class MainActivity : AppCompatActivity() {
             //TextViewに反映
             count_text_view.text = number.toString()
 
+            //効果音の巻き戻し
+            plusSound.seekTo(0)
+            //効果音の再生
+            plusSound.start()
+
             //numberの値によって文字の色を変更
-            if(number >= 100){
+            if(number >= 30){
                 count_text_view.setTextColor(Color.RED)
-            } else if (number >= 50) {
+            } else if (number >= 10) {
                 count_text_view.setTextColor(Color.GREEN)
             } else if (number < 0) {
                 count_text_view.setTextColor(Color.BLUE)
@@ -44,10 +54,15 @@ class MainActivity : AppCompatActivity() {
             //TextViewに反映
             count_text_view.text = number.toString()
 
+            //効果音の巻き戻し
+            minusSound.seekTo(0)
+            //効果音の再生
+            minusSound.start()
+
             //numberの値によって文字の色を変更
-            if(number >= 100){
+            if(number >= 30){
                 count_text_view.setTextColor(Color.RED)
-            } else if (number >= 50) {
+            } else if (number >= 10) {
                 count_text_view.setTextColor(Color.GREEN)
             } else if (number < 0) {
                 count_text_view.setTextColor(Color.BLUE)
